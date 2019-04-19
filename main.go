b4353bb8143ac1422ddb8a225c1b2f67e5da5e42
Chris Mikkelson <chris@mikk.net>
2019-04-19 11:24:17 -0500

    Refactor main logic for slightly better readability.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -12,8 +13,6 @@ import (
 	"strconv"
 	"strings"
 	"syscall"
-
-	"golang.org/x/crypto/ssh"
 )
 
 type proxyConfig struct {
@@ -34,7 +33,7 @@ func printConfig(pc proxyConfig) {
 	fmt.Printf("echo sshttp running, pid %d;\n", pc.ProxyPid)
 }
 
-func runWithConfig(pc proxyConfig, command []string) error {
+func runWithConfig(pc proxyConfig, command []string) {
 	os.Setenv("http_proxy", pc.ProxyAddr)
 	os.Setenv("https_proxy", pc.ProxyAddr)
 	os.Setenv("PROXY_PID", strconv.FormatInt(int64(pc.ProxyPid), 10))
@@ -42,7 +41,12 @@ func runWithConfig(pc proxyConfig, command []string) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	if err := cmd.Run(); err != nil {
+		if _, ok := err.(*exec.ExitError); !ok {
+			log.Println("command: ", err)
+		}
+		os.Exit(1)
+	}
 }
 
 func main() {
@@ -66,61 +70,49 @@ func main() {
 
 	flag.Parse()
 
-	var pc proxyConfig
-	if *query {
-		pc, err = queryConfig(*port)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if *kill {
-			if err = syscall.Kill(pc.ProxyPid, syscall.SIGKILL); err != nil {
-				log.Fatal("Kill failed: ", err.Error())
-			}
-			return
-		}
-
-		command := flag.Args()
-		if len(command) > 0 {
-			if err = runWithConfig(pc, command); err != nil {
-				if _, ok := err.(*exec.ExitError); !ok {
-					log.Println("command: ", err)
-				}
-				os.Exit(1)
-			}
-			return
-		}
-
-		printConfig(pc)
-		return
+	pc := proxyConfig{
+		ProxyAddr: fmt.Sprintf("localhost:%d", *port),
+		ProxyPid:  os.Getpid(),
 	}
 
-	pid := os.Getpid()
+	epid, err := strconv.Atoi(os.Getenv("SSHTTP_PID"))
+	if err == nil {
+		pc.ProxyPid = epid
+	}
+
+	if *query {
+		pc, err = queryConfig(*port)
+		switch {
+		case err != nil:
+			log.Fatal("query: ", err)
+		case *kill:
+			break
+		case flag.NArg() == 0:
+			printConfig(pc)
+			return
+		default:
+			runWithConfig(pc, flag.Args())
+			return
+		}
+	}
+
 	if *kill {
-		envpid := os.Getenv("SSHTTP_PID")
-		if envpid == "" {
-			log.Fatal("SSHTTP_PID not set, exiting.")
+		if pc.ProxyPid == os.Getpid() {
+			log.Fatal("kill: unable to determine proxy pid")
 		}
-		pid, err = strconv.Atoi(envpid)
-		if err != nil {
-			log.Fatalf("Invalid SSHTTP_PID value '%s': %v", envpid, err)
-		}
-		err = syscall.Kill(pid, syscall.SIGKILL)
-		if err != nil {
+
+		if err = syscall.Kill(pc.ProxyPid, syscall.SIGKILL); err != nil {
 			log.Fatal("Kill failed: ", err.Error())
 		}
 		return
 	}
 
-	pc.ProxyAddr = fmt.Sprintf("localhost:%d", *port)
-	pc.ProxyPid = pid
-
-	hostname := flag.Arg(0)
-
-	if hostname == "" {
-		log.Fatal("No ssh hostname specified.")
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(1)
 	}
 
+	hostname := flag.Arg(0)
 	command := flag.Args()[1:]
 
 	if strings.Index(hostname, "@") > 0 {
@@ -139,77 +131,70 @@ func main() {
 	// fork() result.
 	bgProxy := (os.Getenv(fmt.Sprintf("SSHTTP_%d", os.Getppid())) != "")
 
-	if len(command) > 0 || bgProxy {
-		// We are running a command or a background proxy version of
-		// ourselves. In either case, we need to set up the ssh client
-		// connection and HTTP listener.
-		var sshc *ssh.Client
-		var l net.Listener
+	if !bgProxy && len(command) == 0 {
+		var outp io.ReadCloser
+		var b []byte
 
-		sshc, err = sshClient(*username, hostname)
+		// We are not a background proxy process, and have no command
+		// to run, so we start up a background proxy process and print
+		// a proxy config.
+		os.Setenv(fmt.Sprintf("SSHTTP_%d", os.Getpid()), "1")
+
+		cmd := exec.Command(os.Args[0],
+			"-port", strconv.FormatInt(int64(*port), 10),
+			"-user", *username,
+			hostname)
+		outp, err = cmd.StdoutPipe()
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		l, err = net.Listen("tcp", pc.ProxyAddr)
+		cmd.Stderr = os.Stderr
+		cmd.Start()
+		b, err = ioutil.ReadAll(outp)
 		if err != nil {
-			log.Fatal("http: ", err)
+			log.Fatal(err)
 		}
-
-		if bgProxy {
-			// If we are running the background proxy version of
-			// ourselves, we inform our parent of our successful
-			// startup by printing the string "OK" to Stdout, then
-			// start the proxy process, and exit when that finishes
-			// or fails..
-			os.Stdout.Write([]byte("OK\n"))
-			os.Stdout.Close()
-			err = httpProxy(sshc, l)
-			if err != nil {
-				log.Fatal("proxy: ", err)
-			}
-			return
-		}
-
-		// Otherwise, if we are running the command directly,
-		// we start the proxy process in a separate goroutine,
-		// then run the command with the proper environment.
-		go func() {
-			log.Fatal("proxy: ", httpProxy(sshc, l))
-		}()
-		if err = runWithConfig(pc, command); err != nil {
-			if _, ok := err.(*exec.ExitError); !ok {
-				log.Println("command: ", err)
-			}
+		if len(b) == 0 {
 			os.Exit(1)
+		}
+		pc.ProxyPid = cmd.Process.Pid
+		printConfig(pc)
+		return
+	}
+
+	// We are running a command or a background proxy version of
+	// ourselves. In either case, we need to set up the ssh client
+	// connection and HTTP listener.
+	sshc, err := sshClient(*username, hostname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l, err := net.Listen("tcp", pc.ProxyAddr)
+	if err != nil {
+		log.Fatal("http: ", err)
+	}
+
+	if bgProxy {
+		// If we are running the background proxy version of
+		// ourselves, we inform our parent of our successful
+		// startup by printing the string "OK" to Stdout, then
+		// start the proxy process, and exit when that finishes
+		// or fails..
+		os.Stdout.Write([]byte("OK\n"))
+		os.Stdout.Close()
+		err = httpProxy(sshc, l)
+		if err != nil {
+			log.Fatal("proxy: ", err)
 		}
 		return
 	}
 
-	// If we reach here, we are starting up a proxy process in the
-	// background. To do this, we set our sentinel environment variable
-	// and run ourselves (os.Args[0]) with the desired listener and ssh
-	// options. Then we wait for the child process to notify us that it
-	// has started up correctly, print the configuration for using the
-	// child process, then exit.
-	os.Setenv(fmt.Sprintf("SSHTTP_%d", os.Getpid()), "1")
-	cmd := exec.Command(os.Args[0],
-		"-port", strconv.FormatInt(int64(*port), 10),
-		"-user", *username,
-		hostname)
-	outp, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	cmd.Stderr = os.Stderr
-	cmd.Start()
-	b, err := ioutil.ReadAll(outp)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(b) == 0 {
-		os.Exit(1)
-	}
-	pc.ProxyPid = cmd.Process.Pid
-	printConfig(pc)
+	// Otherwise, if we are running the command directly,
+	// we start the proxy process in a separate goroutine,
+	// then run the command with the proper environment.
+	go func() {
+		log.Fatal("proxy: ", httpProxy(sshc, l))
+	}()
+	runWithConfig(pc, command)
 }
